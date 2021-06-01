e2eb1bec9fd1ed26b6bbe224c095e88fc8ca4b85
Bryner Ramirez <jhosepramirez-14@hotmail.com>
2021-06-01 13:55:25 -0500

    Time y new design

--- app/src/main/java/com/cialc/MainActivity2.java
+++ app/src/main/java/com/cialc/MainActivity2.java
@@ -1,10 +1,12 @@
 package com.cialc;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,11 +22,14 @@ import android.widget.Toast;
 
 import com.cialc.localConnections.VolleyConnection;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
     Button buttonHora1, buttonHora2;
-    TextView textViewH1,textViewH2;
+    TextView textViewH1,textViewH2, textViewH3;
     Switch switchHorario, switchTrans;
     ImageView btnback;
     TextView txtHost;
@@ -33,14 +38,19 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     private Object v;
     private int hora, minutos;
     private int hora2, minutos2;
-
-
-
+    private String getTime() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        return dateFormatter.format(calendar.getTime());
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //-----------------------------------------------------------
 
+        //-----------------------------------------------------------
         btnback = (ImageView) findViewById(R.id.btn_back);
         btnback.setOnClickListener(v -> {
             finish();
@@ -115,6 +125,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                 });
             }
         });
+
         //----------------------------------------------------------------------------------------
         int pIntensidad = Integer.parseInt(intent.getStringExtra("progressIntensidad"));
         int pTemperatura = Integer.parseInt(intent.getStringExtra("progressTemperatura"));
@@ -125,6 +136,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         buttonHora2 = (Button) findViewById(R.id.buttonHora2);
         textViewH1 = (TextView) findViewById(R.id.textViewH1);
         textViewH2 = (TextView) findViewById(R.id.textViewH2);
+        textViewH3 = (TextView) findViewById(R.id.textViewH3);
         buttonHora1.setOnClickListener(this);
         buttonHora2.setOnClickListener(this);
         switchHorario =(Switch)findViewById(R.id.switchHorario);
@@ -146,7 +158,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        if (v == buttonHora1 && switchHorario.isChecked() || switchTrans.isChecked()) {
+        if (v == buttonHora1 && switchHorario.isChecked()) {
             final Calendar c = Calendar.getInstance();
             hora = c.get(Calendar.HOUR_OF_DAY);
             minutos = c.get(Calendar.MINUTE);
@@ -158,7 +170,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
             }, hora, minutos, false);
             timePickerDialog.show();
         }
-        if(v == buttonHora2 && switchHorario.isChecked() || switchTrans.isChecked()){
+        if(v == buttonHora2 && switchHorario.isChecked()){
             final Calendar c = Calendar.getInstance();
             hora2 = c.get(Calendar.HOUR_OF_DAY);
             minutos2 = c.get(Calendar.MINUTE);
@@ -170,6 +182,18 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
             }, hora2, minutos2, false);
             timePickerDialog.show();
         }
-    }
+        if (v == buttonHora1 && switchTrans.isChecked()) {
+            final Calendar c = Calendar.getInstance();
+            hora = c.get(Calendar.HOUR_OF_DAY);
+            minutos = c.get(Calendar.MINUTE);
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay3, int minute3) {
+                    textViewH3.setText(hourOfDay3+ ":" +minute3);
+                }
+            }, hora, minutos, false);
+            timePickerDialog.show();
+        }
 
+    }
 }
