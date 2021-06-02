d476a199d343474991a39b0c70414ecdc826df04
Bryner Ramirez <jhosepramirez-14@hotmail.com>
2021-06-02 16:54:12 -0500

    time trans, ini, fin add

--- app/src/main/java/com/cialc/MainActivity2.java
+++ app/src/main/java/com/cialc/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Build;
@@ -28,8 +29,8 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
-    Button buttonHora1, buttonHora2;
-    TextView textViewH1,textViewH2, textViewH3;
+    Button buttonHora1, buttonHora2, buttonSave;
+    TextView textViewH1,textViewH2, textViewH3, tiempo;
     Switch switchHorario, switchTrans;
     ImageView btnback;
     TextView txtHost;
@@ -40,7 +41,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     private int hora2, minutos2;
     private String getTime() {
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm", Locale.getDefault());
         return dateFormatter.format(calendar.getTime());
     }
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -48,9 +49,6 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        //-----------------------------------------------------------
-
-        //-----------------------------------------------------------
         btnback = (ImageView) findViewById(R.id.btn_back);
         btnback.setOnClickListener(v -> {
             finish();
@@ -134,13 +132,33 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         //----------------------------------------------------------------------------------------
         buttonHora1 = (Button) findViewById(R.id.buttonHora1);
         buttonHora2 = (Button) findViewById(R.id.buttonHora2);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
         textViewH1 = (TextView) findViewById(R.id.textViewH1);
         textViewH2 = (TextView) findViewById(R.id.textViewH2);
         textViewH3 = (TextView) findViewById(R.id.textViewH3);
         buttonHora1.setOnClickListener(this);
         buttonHora2.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
         switchHorario =(Switch)findViewById(R.id.switchHorario);
         switchTrans = (Switch)findViewById(R.id.switchTrans);
+        //-------------------------------------------------------------------------------------
+        tiempo = (TextView)findViewById(R.id.tiempo);
+        String tiempoo = getTime();
+        tiempo.setText(getTime());
+        String time = "/data?tiempo=" + String.valueOf(tiempoo) + "&";
+        String urlFinal = url + time;
+        VolleyConnection.getInstance(getApplicationContext()).setRequest(urlFinal, new VolleyConnection.IVolleyResponse() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+
+            }
+        });
+        //--------------------------------------------------------------------------------------------
         switchHorario.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -154,7 +172,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
             }
         });
     }
-
+        //----------------------------------------------------------------------------------------------
 
     @Override
     public void onClick(View v) {
@@ -166,6 +184,19 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                 @Override
                 public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                     textViewH1.setText(hourOfDay+ ":" +minute);
+                    String ini = "/data?TiempoInicio=" + String.valueOf(hourOfDay+":"+minute) + "&";
+                    String urlFinal = url + ini;
+                    VolleyConnection.getInstance(getApplicationContext()).setRequest(urlFinal, new VolleyConnection.IVolleyResponse() {
+                        @Override
+                        public void onResponse(String response) {
+
+                        }
+
+                        @Override
+                        public void onError(String errorMessage) {
+
+                        }
+                    });
                 }
             }, hora, minutos, false);
             timePickerDialog.show();
@@ -178,6 +209,19 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                 @Override
                 public void onTimeSet(TimePicker view, int hourOfDay2, int minute2) {
                     textViewH2.setText(hourOfDay2+ ":" +minute2);
+                    String fin = "/data?TiempoFin=" + String.valueOf(hourOfDay2+":"+minute2) + "&";
+                    String urlFinal = url + fin;
+                    VolleyConnection.getInstance(getApplicationContext()).setRequest(urlFinal, new VolleyConnection.IVolleyResponse() {
+                        @Override
+                        public void onResponse(String response) {
+
+                        }
+
+                        @Override
+                        public void onError(String errorMessage) {
+
+                        }
+                    });
                 }
             }, hora2, minutos2, false);
             timePickerDialog.show();
@@ -190,6 +234,19 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                 @Override
                 public void onTimeSet(TimePicker view, int hourOfDay3, int minute3) {
                     textViewH3.setText(hourOfDay3+ ":" +minute3);
+                    String trans = "/data?TiempoTrans=" + String.valueOf(hourOfDay3+":"+minute3) + "&";
+                    String urlFinal = url + trans;
+                    VolleyConnection.getInstance(getApplicationContext()).setRequest(urlFinal, new VolleyConnection.IVolleyResponse() {
+                        @Override
+                        public void onResponse(String response) {
+
+                        }
+
+                        @Override
+                        public void onError(String errorMessage) {
+
+                        }
+                    });
                 }
             }, hora, minutos, false);
             timePickerDialog.show();
